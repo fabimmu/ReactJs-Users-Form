cc494f82ead56f28aabb1af11d6bc22eeb31e549
Fabiola Montoya <fmontoya@cecropiasoltuions.com>
2021-10-11 22:28:05 -0600

    Form filtering to exclude users

---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import classes from './User.module.css';
+
+class User extends Component {
+  componentWillUnmount() {
+    console.log('User will unmount!');
+  }
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+//ES LO MISMO
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
+
+export default User;
